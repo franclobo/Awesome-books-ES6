a2076869f5909eaa2a543a75752d5e1793b8179b
franclobo <fjbl2788@gmail.com>
2022-05-03 18:59:24 -0500

    Add clock.js and variables.js

---
+++ modules/variables.js
@@ -0,0 +1,5 @@
+export const books = document.getElementById('books');
+export const addNew = document.getElementById('addbook');
+export const contact = document.getElementById('contact');
+export const titulo = document.getElementById('titulo');
+export const autor = document.getElementById('autor');
