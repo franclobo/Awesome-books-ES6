0104868b62dc60ccb236a0b7f1ffe83f877e437e
franclobo <fjbl2788@gmail.com>
2022-05-03 09:57:37 -0500

    Add luxon and pakage-lock.json

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -10,41 +10,35 @@ const autor = document.getElementById('autor');
 const addBtn = document.getElementById('btn');
 let dataBooks = JSON.parse(localStorage.getItem('book')) || [];
 
-// This code below was recovered from https://programacion.net/
-function checkTime(i) {
-  if (i < 10) {
-    i = `0${i}`;
-  }
-  return i;
-}
-
-function startTime() {
-  const today = new Date();
-  let hr = today.getHours();
-  let min = today.getMinutes();
-  let sec = today.getSeconds();
-  const ap = (hr < 12) ? '<span>AM</span>' : '<span>PM</span>';
-  hr = (hr === 0) ? 12 : hr;
-  hr = (hr > 12) ? hr - 12 : hr;
-  hr = checkTime(hr);
-  min = checkTime(min);
-  sec = checkTime(sec);
-  document.getElementById('clock').innerHTML = `${hr}:${min}:${sec} ${ap}`;
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const curWeekDay = days[today.getDay()];
-  const curDay = today.getDate();
-  const curMonth = months[today.getMonth()];
-  const curYear = today.getFullYear();
-  const date = `${curWeekDay}, ${curDay} ${curMonth} ${curYear}`;
-  document.getElementById('date').innerHTML = date;
-  setTimeout(() => { startTime(); }, 1000);
-}
-// This code above was recovered from https://programacion.net/
+import { DateTime } from "./luxon.js";
 
 window.onload = startTime();
 
-import { Libro, printList } from "./JavaScript/list.js";
+import Libro from "./JavaScript/list.js";
+
+const printList = () => {
+  const libro = new Libro();
+  books.innerHTML = null;
+  if (localStorage.getItem('book')) {
+    dataBooks = JSON.parse(localStorage.getItem('book')) || [];
+  }
+  for (let i = 0; i < dataBooks.length; i += 1) {
+    const bookStorage = document.createElement('div');
+    const bookInfo = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+    bookInfo.textContent = `"${dataBooks[i].title}" by ${dataBooks[i].author}`;
+    deleteBtn.textContent = 'Remove';
+    books.appendChild(bookStorage);
+    bookStorage.append(bookInfo, deleteBtn);
+    bookStorage.className = 'bookStorage';
+    deleteBtn.addEventListener('click', () => {
+      bookStorage.remove();
+      libro.deleteBook(i);
+    });
+  }
+};
+
+printList();
 
 addBtn.addEventListener('click', () => {
   const libro = new Libro(titulo.value, autor.value);
